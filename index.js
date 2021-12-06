848afb202b3664a852491b6c7cb34c59485d07dc
SaraMunir <sara.m07011@gmail.com>
2021-12-05 22:19:58 -0500

    adding portfolio projects

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
         console.log(document.body.scrollTop)
-    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
         $('#sideBar').addClass( "sideWidthSmll" )
         $('#sideBar').removeClass( "sideWidthBig" )
         
     } else {
         $('#sideBar').addClass( "sideWidthBig" )
         $('#sideBar').removeClass( "sideWidthSmll" )
+    }
+    if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
+        $('#profileImgC').attr("src", "./assets/myImg4.png");
+        // $('#sideBar').removeClass( "sideWidthBig" )
+    }else {
+        $('#profileImgC').attr("src", "./assets/myImg1.png");
 
     }
 }
@@ -31,3 +37,19 @@ $(document).ready(function(e){
 var textWrapper = document.querySelector('.ml3');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
+
+function myScroll(type){
+    if(type === 'aboutSumm'){
+        $('html, body').animate({
+            scrollTop: $("#aboutSumm").offset().top
+        }, 1000);
+    }
+}
+const showContent=(type)=>{
+    if(type=== 'aboutMe'){
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    }
+
+}
