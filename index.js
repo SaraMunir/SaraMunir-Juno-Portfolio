f3af1d951d1ac801f37d1d810d4f6c91449ed7d5
SaraMunir <sara.m07011@gmail.com>
2021-12-07 13:08:11 -0500

    updated resume

--- index.js
+++ index.js
@@ -35,7 +35,62 @@ const showContent=(type)=>{
             scrollTop: $("#about").offset().top
         }, 1000);
     }
+}
+const openModal = (idx, imgIdx)=>{
+    $('#modal').show( "slow" )
+    renderModalImg(idx, imgIdx)
+}
+const focusThisImg = (idx, imgIdx)=>{
+    // $('#modal').show( "slow" )
+    console.log('is it working?')
+    renderModalImg(idx, imgIdx)
+}
 
+const renderModalImg =(idx, imgIdx)=>{
+    $('#focusedImgs').html('')
+    $('#focusedImgs').html(`
+        <i class="fas fa-chevron-left" onclick="prevImg('${idx}','${imgIdx}')"></i>
+        <div class='fixedImgwidth'>
+            <img id="imgInFocus" src="${projects[idx].images[imgIdx]}" alt="image ${imgIdx} of the ${projects[idx].name}">
+        </div>
+        <i class="fas fa-chevron-right" onclick="nextImg('${idx}','${imgIdx}')"></i>
+    `)
+    $('.allImages').html('')
+    projects[idx].images.forEach((image, idxs)=>{
+        if(imgIdx == idxs){
+            $('.allImages').append(`
+            <img class="spcl" src='${image}' alt='image of ${projects[idx].name}'>
+            `)
+        }else {
+            $('.allImages').append(`
+            <img src='${image}' alt='image of ${projects[idx].name}' onclick="focusThisImg('${idx}','${idxs}')">
+            `)
+        }
+    })
+}
+const nextImg =(idx, imgIdx)=>{
+    console.log(imgIdx)
+    console.log(projects[idx].images.length)
+    if(imgIdx < projects[idx].images.length-1){
+        renderModalImg(idx , (Number(imgIdx)+1) )
+    }else {
+        console.log('no more image availalbe')
+        renderModalImg(idx , 0 )
+        return
+    }
+}
+const prevImg =(idx, imgIdx)=>{
+    if(imgIdx > 0){
+        renderModalImg(idx , (Number(imgIdx)-1) )
+    }else {
+        console.log('no more image availalbe')
+        renderModalImg(idx , projects[idx].images.length-1  )
+        return
+    }
+
+}
+const closeModal = ()=>{
+    $('#modal').hide( "slow" )
 }
 const showDetail=(id)=>{
     console.log('show detail')
@@ -45,18 +100,73 @@ const showDetail=(id)=>{
     $('#projectName').text(projects[id].name)
     $('#projectDescription').text(projects[id].description)
     $('#projectImgs').html('')
-    projects[id].images.forEach(image=>{
+    // $('.imageArray').html('')
+    $('.stackArray').html('')
+    projects[id].images.forEach((image, idx)=>{
         console.log(image)
         $('#projectImgs').append(`
-        <img src=${image} alt="">
+        <img onclick="openModal('${id}','${idx}')" src='${image}' alt='image of ${projects[id].name}'>
         `)
     })
+    // ['REACT', 'firebase', 'css', 'html', 'javascript']
+    projects[id].stacks.forEach(stack=>{
+        if(stack === 'REACT'){
+            $('.stackArray').append(`
+            <i class="fab fa-react"></i>
+            `)
+        }
+        if(stack === 'css'){
+            $('.stackArray').append(`
+            <i class="fab fa-css3-alt"></i>
+            `)
+        }
+        if(stack === 'sass'){
+            $('.stackArray').append(`
+            <i class="fab fa-sass"></i>
+            `)
+        }
+        if(stack === 'html'){
+            $('.stackArray').append(`
+            <i class="fab fa-html5"></i>
+            `)
+        }
+        if(stack === 'javascript'){
+            $('.stackArray').append(`
+            <i class="fab fa-js"></i>
+            `)
+        }
+        if(stack === 'firebase'){
+            $('.stackArray').append(`
+            <img src="./assets/fireBasePink.png" alt="firebase db icon">
+            `)
+        }
+        if(stack === 'mongodb'){
+            $('.stackArray').append(`
+            <img src="./assets/mongoLogoPink.png" alt="mongo db icon">
+            `)
+        }
+        if(stack === 'nodejs'){
+            $('.stackArray').append(`
+            <i class="fab fa-node"></i>
+            `)
+        }
+    })
+}
+const goBackToPortfolio=()=>{
+    $('#portfolio').show( "slow" )
+    $('#portfolioDetail').hide( "slow" )
+    $('#projectName').text('')
+    $('#projectDescription').text('')
+    $('#projectImgs').html('')
+    // $('.imageArray').html('')
+    $('.stackArray').html('')
+
 }
 const projects=[
     {
         id: 0,
         name: 'Sun Run',
-        description: 'Sun Run is a fitness app that allows users to plan their runs based on sunlight. User can select if they want to hit sunrise or sunset and plan their runs accordingly. Based on the users selection the app suggests when they should start their run to hit sunrise or sunset.',
+        description: 'Sun Run is a fitness app that allows users to schedule their runs based on sunlight. User can select if they want to hit sunrise or sunset and plan their runs accordingly. Based on the users selection the app suggests when they should start their run to hit sunrise or sunset.',
         goal: 'This agency-style application was built on REACT,in a group project assigned by Juno College as part of their curriculum.  / The main goal of the project was to mimick the agency environment working in small groups to build web applications for clients. ',
         requirements: [
             "Using the Sunrise/Sunset API - https://sunrise-sunset.org/api - suggest the time the user should leave for their run in order to hit sunrise or be back before sunset",
@@ -67,12 +177,12 @@ const projects=[
             {
                 name: 'Adrienne Lee',
                 github: 'adrienneklee',
-                website: ''
+                website: 'https://adriennelee.dev/'
             }, 
             {
                 name:'Dallan Jones',
                 github:'dallanj',
-                website: ''
+                website: 'https://dallan.ca/'
             }, 
             {
                 name:'Kelvin Lee',
@@ -92,12 +202,13 @@ const projects=[
             './assets/sunRunImg1.png',
             './assets/sunRunImg2.png',
             './assets/sunRunImg3.png',
-        ]
+        ], 
+        stacks: ['REACT', 'firebase', 'css', 'html', 'javascript']
     },
     {
         id: 1,
         name: 'Sophies Blog',
-        description: "This is a website for Sophie's blog.",
+        description: "A multipage website for Sophie's blog. Where she blogs about her daily life. The website is fully responsive on all screen sizes and has a collapsing navigation menu",
         goal: 'To convert a static design into a multi-page, functional, responsive website.',
         requirements: [
             "Bring the designer’s vision to life with a fully-functional website using HTML and CSS techniques",
@@ -117,7 +228,61 @@ const projects=[
             './assets/sophiesBlogImg1.png',
             './assets/sophiesBlogImg2.png',
             './assets/sophiesBlogImg3.png'
-        ]
+        ],
+        stacks: ['css', 'html', 'sass']
+    },
+    {
+        id: 2,
+        name: 'Thought Escape',
+        description: "A social media application based off facebook/twitter that allows a user to share their thoughts. Users can follow other users, comment on posts and like thoughts they or others have posted. /",
+        goal: 'To showcase an understanding of core React concepts (ie. state, props, components), working with external or third-party data sources (ie. APIs / Firebase), error handling and UI design..',
+        requirements: [
+            "It is clear to the user what the app does and results are displayed legibly",
+            "App is dynamic based on user interaction (e.g. drop down menu, search field)", 
+            "App and interactions are accessible"
+        ],
+        collaborators: [
+            {
+                name: 'Sara Munir',
+                github:'SaraMunir',
+                website: 'https://saramunir.com/'
+            }
+        ],
+        createdOn: '',
+        website: 'https://thought-scape.herokuapp.com/',
+        github: "https://github.com/SaraMunir/sara-munir-juno-project3",
+        images: [
+            './assets/thoughtEscapeImgs1.png',
+            './assets/thoughtEscapeImgs2.png',
+            './assets/thoughtEscapeImgs3.png'
+        ],
+        stacks: ['REACT', 'firebase', 'css', 'html', 'javascript']
+    },
+    {
+        id: 3,
+        name: 'Ecommerce Website',
+        description: "An eccomerce website that allows users to shop clotthing of their choices. / users can select clothing and add to cart. They can only select upto 5 of each size as maximum number. The quantity is based on stock available. if there are any clothing with zero amount, users are not able to select. / Users cart are saved in local storage when they come back. Once they select, they are then prompted to provide payment and shipping information. Upon completion they are provided with the order confirmation",
+        goal: 'To showcase the usage of local storage, backend integration with MongoDb along with successfully incorporating react components such as react routing, useState and useEffect',
+        requirements: [],
+        collaborators: [
+            {
+                name: 'Sara Munir',
+                github:'SaraMunir',
+                website: 'https://saramunir.com/'
+            }
+        ],
+        createdOn: '',
+        website: 'https://my-ecommerce-web.herokuapp.com/',
+        github: "https://github.com/SaraMunir/myEcommerce",
+        images: [
+            './assets/eccomerseStitchesImgs1.png',
+            './assets/eccomerseStitchesImgs2.png',
+            './assets/eccomerseStitchesImgs3.png',
+            './assets/eccomerseStitchesImgs4.png',
+            './assets/eccomerseStitchesImgs5.png',
+            './assets/eccomerseStitchesImgs6.png',
+        ],
+        stacks: ['REACT', 'css', 'html', 'javascript', 'mongodb', 'nodejs', ]
     }
 ]
 $(document).ready(function(e){
